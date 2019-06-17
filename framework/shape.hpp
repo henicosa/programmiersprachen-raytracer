d35d8d5cf22002339ed0b73896e5f872693121cb
henicosa <ludwig.david.lorenz@uni-weimar.de>
2019-06-17 15:53:13 +0200

    added exercise 5.1

---
+++ framework/shape.hpp
@@ -0,0 +1,13 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+class Shape {
+  public:
+    virtual float area() const = 0;
+    virtual float volume() const = 0;
+
+  private:
+
+};
+
+#endif // shape.hpp
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,19 @@
 #define CATCH_CONFIG_RUNNER
+// Include von lokalem / projektinternem File
+#include "shape.hpp"
+
+// Include von File aus einer Fremdbibliothek
 #include <catch.hpp>
+
+// Standard - Headers
+#include <iostream>
+
+
+// exercise 5.1
+TEST_CASE("virtual shape", "[virtual_shape]" ) 
+{
+
+}
 
 int main(int argc, char *argv[])
 {
